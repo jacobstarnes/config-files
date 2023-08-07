960a1cb9acf77bdd598cec43943713b382ab762f
jacobstarnes <southplainssolutions@gmail.com>
2023-08-07 17:25:13 +0300

    .

---
+++ www/public/product1/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Product 1</title>
+<script src=" https://cdn.jsdelivr.net/npm/@picocss/pico@1.5.10/css/postcss.config.min.js "></script>
+<link href=" https://cdn.jsdelivr.net/npm/@picocss/pico@1.5.10/css/pico.min.css " rel="stylesheet">
+	</head>
+	<body>
+<div class="container">
+<h1>Product 1</h1>
+</div>
+	</body>
+</html>
+
